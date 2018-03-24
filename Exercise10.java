d2fbcccfe16b55127a81f5c662d5b85110326f68
Wen Sun <syl1887415157@126.com>
2018-03-24 21:12:39 +0800

    print a string

---
+++ Exercise10.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.*;
+
+public class Exercise10 {
+	
+	private static Scanner input;
+
+	public static void main(String[] args) {
+		input = new Scanner(System.in);
+		String s=input.nextLine();
+		System.out.println(s);
+	}
+}
