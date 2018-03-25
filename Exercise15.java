573572bc9ac9fd23ee5419afb317126238aed06e
Wen Sun <syl1887415157@126.com>
2018-03-25 09:07:44 +0800

    comments

---
+++ Exercise15.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Exercise15 {
+	public static void main(String[] args) {
+		//This is a comment
+		/*This is also a comment*/
+		/*This is 
+		 * still
+		 * a comment
+		 */
+	}
+}
